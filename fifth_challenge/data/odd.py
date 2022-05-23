50eb984f95ce7c78bc5ddd6d37062a05c542eca5
Isioma Iloh <davidiloh@ymail.com>
2022-05-24 00:36:50 +0300

    my ci

---
+++ fifth_challenge/data/odd.py
@@ -0,0 +1,6 @@
+# third_challenge/lib.py
+
+def odd(number):
+    if number % 2 != 0:
+        return True
+    return False
---
+++ tests/test_odd.py
@@ -0,0 +1,8 @@
+from typing_extensions import assert_type
+from fifth_challenge.data import odd
+
+def test_odd():
+    number = 4
+    assert odd(number) == False
+    result = odd(number)
+    assert isinstance(result, bool)
